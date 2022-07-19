3607c5106138cf3f834965789b850b7eae18987a
Zsudoku <1186157045@qq.com>
2022-07-19 19:34:42 +0800

    更新

--- .history/Pistachio_Dataset/main_20220719153354.py
+++
@@ -1,7 +0,0 @@
-'''
-Date: 2022-07-19 15:33:48
-LastEditors: ZSudoku
-LastEditTime: 2022-07-19 15:33:48
-FilePath: \Pistachio_DeepLearning\Pistachio_Dataset\main.py
-'''
-from f
--- .history/fileload_20220719153258.py
+++
@@ -1,7 +0,0 @@
-'''
-Date: 2022-07-19 15:32:28
-LastEditors: ZSudoku
-LastEditTime: 2022-07-19 15:32:47
-FilePath: \Pistachio_DeepLearning\fileload.py
-'''
-dataset = './Pi'
