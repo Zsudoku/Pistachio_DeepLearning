7eee6d0b08093146c98a074a9105c6585287e188
Zsudoku <1186157045@qq.com>
2022-07-21 22:33:36 +0800

    bp

--- fileload.py
+++ fileload.py
@@ -1,7 +1,8 @@
 '''
 Date: 2022-07-19 15:32:28
 LastEditors: ZSudoku
-LastEditTime: 2022-07-19 15:32:47
+LastEditTime: 2022-07-21 22:32:53
 FilePath: \Pistachio_DeepLearning\fileload.py
 '''
 dataset = './Pistachio_Dataset/Pistachio_28_Features_Dataset/Pistachio_28_Features_Dataset.xls'
+
